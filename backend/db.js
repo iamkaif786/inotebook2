8bcf689aed3ef0d56684b1b58158d574ae206a32
Mohammadkaif Shaikh <skaif054@gmail.com>
2023-05-04 12:00:43 +0530

    Created Routes & Schemas

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://127.0.0.1:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = ()=> {
-   mongoose.connect(mongoURI).then(console.log("Connected to Mongo Successfully"))
+   mongoose.connect(mongoURI).then(console.log("Connected to MongoDB Successfully"))
   .catch (error => console.log(error));
 }
 
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
+
 connectToMongo();
+const app = express();
+const port = 5000;
 
-const app = express()
-const port = 5000
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-app.get('/', (req, res) => {
-  res.send('Hello Kaif!')
-})
 
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+  app.listen(port, () => {
+    console.log(`iNotebook backend listening at http://localhost:${port}`);
+  });
